2d92e6408adf095cfd060bdf032cecca7dd3a57f
Mubarak <adebowaleraji0103@gmail.com>
2025-11-01 18:37:14 +0100

    d

--- src/Navbar/Mobile.jsx
+++ src/Navbar/Mobile.jsx
@@ -1,12 +1,12 @@
 const Mobile = () => {
   return (
-    <div>
-      <div className="flex gap-7 text-white">
+    <div className="flex justify-between items-center text-white">
+      <div className="flex gap-6 ">
         <img src="/assets/hamburger.svg" alt="hambugger" />
-        <h3 className="font-bold text-xl">audiophile</h3>
+        <h3 className="font-semibold text-xl">audiophile</h3>
       </div>
       <div>
-        <img src="/assets/cart.svg" alt="cart" />
+        <img src="/assets/carts.svg" alt="cart" />
       </div>
     </div>
   );
