6a73ffcb5ec2a0d8d761f39a797cd563fab9e0cc
Mubarak <adebowaleraji0103@gmail.com>
2025-11-01 18:08:38 +0100

    d

---
+++ src/Navbar/Navlink.jsx
@@ -0,0 +1,8 @@
+const NavbarLink = [
+  { label: "Home", href: "/" },
+  { label: "HEADPHONES", href: "/headphones" },
+  { label: "SPEAKERS", href: "/speakers" },
+  { label: "EARPHONES", href: "/earphones" },
+];
+
+export default NavbarLink;
