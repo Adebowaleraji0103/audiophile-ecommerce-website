f3ea803adbf55e68edd4e76c298dbc389bd77795
Mubarak <adebowaleraji0103@gmail.com>
2025-11-01 18:27:35 +0100

    fklf

--- src/Navbar/Mobile.jsx
+++ src/Navbar/Mobile.jsx
@@ -1,9 +1,12 @@
 const Mobile = () => {
   return (
     <div>
+      <div className="flex gap-7 text-white">
+        <img src="/assets/hamburger.svg" alt="hambugger" />
+        <h3 className="font-bold text-xl">audiophile</h3>
+      </div>
       <div>
-        <img src="/assets/hamburger.svg" alt="" />
-        <h1>welcome</h1>
+        <img src="/assets/cart.svg" alt="cart" />
       </div>
     </div>
   );
