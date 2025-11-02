7f639bdc45a538e0363bdae9b6566f284e310be0
Mubarak <adebowaleraji0103@gmail.com>
2025-11-02 12:46:27 +0100

    the headphoneshop section was added

---
+++ src/pages/Homepage/HeadphoneShop.jsx
@@ -0,0 +1,7 @@
+const images = [
+  "/assets/cart/image-xx99-mark-one-headphones.jpg",
+  "/assets/cart/image-xx99-mark-one-headphones.jpg",
+  "/assets/cart/image-xx99-mark-one-headphones.jpg",
+  "/assets/cart/image-xx99-mark-one-headphones.jpg",
+  "/assets/cart/image-xx99-mark-one-headphones.jpg",
+];
