092b2835b3a25dcd1a64ea2b2185c8c06dc581b6
Mubarak <adebowaleraji0103@gmail.com>
2025-11-02 11:09:25 +0100

    d

--- src/Navbar/Desktop.jsx
+++ src/Navbar/Desktop.jsx
@@ -1,13 +1,19 @@
+import NavbarLink from "./Navlink";
+
 const Desktop = () => {
   return (
     <div className="text-white">
       <h3 className="font-semibold text-xl ">audiophile</h3>
 
       <ul>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
+        {NavbarLink.map((link, index) => (
+          <ul key={link}>
+            <li>
+              {link.href}
+              {link.label}
+            </li>
+          </ul>
+        ))}
       </ul>
       <img src="/assets/carts.svg" alt="cart" />
     </div>
