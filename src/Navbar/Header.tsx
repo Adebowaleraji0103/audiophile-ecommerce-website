159a42d43e7555e7d28ec2252e5c92a9ae8822b7
Mubarak <adebowaleraji0103@gmail.com>
2025-11-03 14:22:24 +0100

    i jst finish the mobile section

--- src/Navbar/Header.tsx
+++ src/Navbar/Header.tsx
@@ -3,7 +3,7 @@ import Mobile from "./Mobile";
 
 const Header = () => {
   return (
-    <div className="bg-[#1a1a1a] text-[14px] font-semibold pb-5 border-b border-b-zinc-700 py-5 px-7">
+    <div className="bg-[#1a1a1a] text-[14px] font-semibold pb-5 border-b border-b-zinc-700 py-5 ">
       <Mobile />
       <Desktop />
     </div>
--- src/Navbar/Mobile.tsx
+++ src/Navbar/Mobile.tsx
@@ -1,16 +1,41 @@
+import NavbarLink from "./Navlink";
+import { useState } from "react";
+
 const Mobile = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div className="flex justify-between items-center text-white">
-      <div className="flex gap-6 items-center">
-        <img src="/assets/hamburger.svg" alt="hambugger" className="lg:hidden"/>
-        <h3 className="font-semibold text-xl hidden md:flex lg:hidden">audiophile</h3>
-      </div>
-      <h3 className="font-semibold text-xl md:hidden">audiophile</h3>
-      <div>
-        <img src="/assets/carts.svg" alt="cart" className="lg:hidden"/>
-      </div>
-    </div>
-  );
-};
+<div className="relative flex justify-between items-center text-white px-7">
+  <div className="flex gap-6 items-center">
+    <button onClick={() => setIsOpen(!isOpen)}>
+      <img
+        src="/assets/hamburger.svg"
+        alt="hamburger"
+        className="lg:hidden relative z-50"
+      />
+    </button>
+
+    {isOpen && (
+      <ul className="bg-black font-semibold flex flex-col gap-[2rem] w-full h-screen px-5 py-[2rem] absolute top-11 left-0 z-40 lg:hidden">
+        {NavbarLink.map((link, index) => (
+          <li key={index}>
+            <a href={link.href}>{link.label}</a>
+          </li>
+        ))}
+      </ul>
+    )}
+
+    <h3 className="font-semibold text-xl hidden md:flex lg:hidden">
+      audiophile
+    </h3>
+  </div>
+
+  <h3 className="font-semibold text-xl md:hidden">audiophile</h3>
+
+  <div>
+    <img src="/assets/carts.svg" alt="cart" className="lg:hidden" />
+  </div>
+</div>
+  )}
 
 export default Mobile;
