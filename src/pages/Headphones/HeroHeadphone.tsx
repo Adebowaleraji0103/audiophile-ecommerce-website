ef2b21fd1d3b582fad535565909320eb0137c0b3
Mubarak <adebowaleraji0103@gmail.com>
2025-11-03 14:41:15 +0100

    i jst created the hero section of the headphones page

---
+++ src/pages/Headphones/HeroHeadphone.tsx
@@ -0,0 +1,23 @@
+const card = [
+  {
+    imageMobile: "/assets/shared/mobile/image-xx99-mark-two-headphones.jpg",
+    imageTablet: "/assets/shared/tablet/image-xx99-mark-two-headphones.jpg",
+    imageDesktop: "/assets/shared/desktop/image-xx99-mark-two-headphones.jpg",
+    label: "NEW PRODUCT",
+    name: "XX99 MARK II HEADPHONES",
+    description:
+      "The new XX99 Mark II headphones is the pinnacle of pristine audio. It redefines your premium headphone experience by reproducing the balanced depth and precision of studio-quality sound.",
+    button: "SEE PRODUCT",
+  },
+];
+
+const HeroHeadphone = () => {
+  return (
+    <main>
+      <h1>HEADPHONES</h1>
+      <div></div>
+    </main>
+  );
+};
+
+export default HeroHeadphone;
