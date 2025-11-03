5e4716a7618f6fe276c8b3f86884c360152eeaf0
Mubarak <adebowaleraji0103@gmail.com>
2025-11-03 14:32:05 +0100

    about to start the headphone page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Footer from "./Footer";
 import Header from "./Navbar/Header";
 import Homepage from "./pages/Homepage/Homepage";
+import Headphones from "./pages/Headphones/Headphone";
 
 const App = () => {
-  return <div>
-    <Header/>
-    <Homepage/>
-    <Footer/>
-  </div>;
+  return (
+    <div>
+      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/headphones" element={<Headphones />} />
+          <Route path="/" element={<Homepage />} />
+        </Routes>
+      </BrowserRouter>
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/pages/Headphones/Headphone.jsx
@@ -0,0 +1,9 @@
+const Headphones = () => {
+    return ( 
+        <div className="bg-amber-400 h-[10rem]"> 
+
+        </div>
+     );
+}
+ 
+export default Headphones;
