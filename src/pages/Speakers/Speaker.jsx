8ee321d73aff6c64b0fda9eea05d416ff0c3067e
Mubarak <adebowaleraji0103@gmail.com>
2025-11-03 15:43:40 +0100

    i jst added the speaker page

---
+++ src/pages/Speakers/Speaker.jsx
@@ -0,0 +1,7 @@
+const Speaker = () => {
+    return ( 
+        
+     );
+}
+ 
+export default Speaker;
---
+++ src/pages/Speakers/SpeakerHomepage.jsx
@@ -0,0 +1,9 @@
+const SpeakerHomepage = () => {
+    return ( 
+        <div>
+            
+        </div>
+     );
+}
+ 
+export default SpeakerHomepage;
