89bc6c6d9078e86c6eb2692f30640574f0566bba
Mubarak <adebowaleraji0103@gmail.com>
2025-11-04 19:03:45 +0100

    d

--- src/Data/product.js
+++ src/Data/product.js
@@ -9,9 +9,11 @@ const products = [
       "The new XX99 Mark II headphones is the pinnacle of pristine audio. It redefines your premium headphone experience by reproducing the balanced depth and precision of studio-quality sound.",
     id: "xx99-mark-two",
     price: 2999,
-    features: "Featuring a genuine leather head strap and premium earcups, these headphones deliver superior comfort for those who like to enjoy endless listening. It includes intuitive controls designed for any situation. Whether you’re taking a business call or just in your own personal space, the auto on/off and pause features ensure that you’ll never miss a beat.",
-    features2:"The advanced Active Noise Cancellation with built-in equalizer allow you to experience your audio world on your terms. It lets you enjoy your audio in peace, but quickly interact with your surroundings when you need to. Combined with Bluetooth 5. 0 compliant connectivity and 17 hour battery life, the XX99 Mark II headphones gives you superior sound, cutting-edge technology, and a modern design aesthetic.",
-     
+    features:
+      "Featuring a genuine leather head strap and premium earcups, these headphones deliver superior comfort for those who like to enjoy endless listening. It includes intuitive controls designed for any situation. Whether you’re taking a business call or just in your own personal space, the auto on/off and pause features ensure that you’ll never miss a beat.",
+    features2:
+      "The advanced Active Noise Cancellation with built-in equalizer allow you to experience your audio world on your terms. It lets you enjoy your audio in peace, but quickly interact with your surroundings when you need to. Combined with Bluetooth 5. 0 compliant connectivity and 17 hour battery life, the XX99 Mark II headphones gives you superior sound, cutting-edge technology, and a modern design aesthetic.",
+
     includes: [
       { quantity: 1, item: "Headphone Unit" },
       { quantity: 2, item: "Replacement Earcups" },
@@ -30,7 +32,10 @@ const products = [
       "As the gold standard for headphones, the classic XX99 Mark I offers detailed and accurate audio reproduction for audiophiles, mixing engineers, and music aficionados alike in studios and on the go.",
     id: "xx99-mark-one",
     price: 1750,
-    features: `As the first model in the XX99 series, the Mark I headphones demonstrate Audiophile's commitment to delivering high-fidelity audio. Featuring a genuine leather head strap and premium earcups, these headphones deliver superior comfort for those who like to enjoy endless listening. It includes intuitive controls designed for ease of use and a stylish aluminum frame.`,
+    features:
+      "As the headphones all others are measured against, the XX99 Mark I demonstrates over five decades of audio expertise, redefining the critical listening experience. This pair of closed-back headphones are made of industrial, aerospace-grade materials to emphasize durability at a relatively light weight of 11 oz. ",
+    features2:
+      "From the handcrafted microfiber ear cushions to the robust metal headband with inner damping element, the components work together to deliver comfort and uncompromising sound. Its closed-back design delivers up to 27 dB of passive noise cancellation, reducing resonance by reflecting sound to a dedicated absorber. For connectivity, a specially tuned cable is included with a balanced gold connector.",
     includes: [
       { quantity: 1, item: "Headphone Unit" },
       { quantity: 2, item: "Replacement Earcups" },
@@ -49,7 +54,10 @@ const products = [
       "Enjoy your audio almost anywhere and customize it to your specific tastes with the XX59 headphones. The stylish yet durable versatile wireless headset is a brilliant companion at home or on the move.",
     id: "xx99-mark-three",
     price: 899,
-    features: `These headphones have been created from durable, high-quality materials tough enough to withstand daily use. Its compact design folds easily for transport and storage, while the earcups swivel to provide a better fit and improved comfort for extended listening sessions.`,
+    features:
+      "These headphones have been created from durable, high-quality materials tough enough to take anywhere. Its compact folding design fuses comfort and minimalist style making it perfect for travel. Flawless transmission is assured by the latest wireless technology engineered for audio synchronization with videos.",
+    features2:
+      "More than a simple pair of headphones, this headset features a pair of built-in microphones for clear, hands-free calling when paired with a compatible smartphone. Controlling music and calls is also intuitive thanks to easy-access touch buttons on the earcups. Regardless of how you use the  XX59 headphones, you can do so all day thanks to an impressive 30-hour battery life that can be rapidly recharged via USB-C.",
     includes: [
       { quantity: 1, item: "Headphone Unit" },
       { quantity: 2, item: "Replacement Earcups" },
--- src/pages/Product Details/ProductDetails.jsx
+++ src/pages/Product Details/ProductDetails.jsx
@@ -76,9 +76,14 @@ const Product = () => {
       <div className="flex flex-col lg:flex-row justify-between gap-10 mt-16">
         <div className="lg:w-2/3">
           <h2 className="text-2xl font-bold mb-4">Features</h2>
-          <p className="text-gray-600 whitespace-pre-line leading-relaxed">
-            {product.features}
-          </p>
+          <div className="flex flex-col gap-[1rem] md:gap-[1.5rem]">
+            <p className="text-gray-600 whitespace-pre-line leading-relaxed">
+              {product.features}
+            </p>
+            <p className="text-gray-600 whitespace-pre-line leading-relaxed">
+              {product.features2}
+            </p>
+          </div>
         </div>
 
         <div className="lg:w-1/3">
