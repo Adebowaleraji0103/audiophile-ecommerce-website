8b8846e58a2e7b80cdf3e0d409be3215d9cc1e59
Mubarak <adebowaleraji0103@gmail.com>
2025-11-04 19:29:42 +0100

    d

--- src/Data/product.js
+++ src/Data/product.js
@@ -66,5 +66,29 @@ const products = [
     ],
     button: "SEE PRODUCT",
   },
+
+  {
+    imageMobile: "/assets/shared/mobile/image-zx9-speaker.jpg",
+    imageTablet: "/assets/shared/tablet/image-zx9-speaker.jpg",
+    imageDesktop: "/assets/shared/desktop/image-zx9-speaker.jpg",
+    label: "NEW PRODUCT",
+    name: "ZX9 SPEAKER",
+    description:
+      "Upgrade your sound system with the all new ZX9 active speaker. It’s a bookshelf speaker system that offers truly wireless connectivity -- creating new possibilities for more pleasing and practical audio setups.",
+    id: "zx9-speaker",
+    price: 4500,
+    features:
+      "Connect via Bluetooth or nearly any wired source. This speaker features optical, digital coaxial, USB Type-B, stereo RCA, and stereo XLR inputs, allowing you to have up to five wired source devices connected for easy switching. Improved bluetooth technology offers near lossless audio quality at up to 328ft (100m).",
+    features2:
+      "Discover clear, more natural sounding highs than the competition with ZX9’s signature planar diaphragm tweeter. Equally important is its powerful room-shaking bass courtesy of a 6.5” aluminum alloy bass unit. You’ll be able to enjoy equal sound quality whether in a large room or small den. Furthermore, you will experience new sensations from old songs since it can respond to even the subtle waveforms.",
+    includes: [
+      { quantity: 2, item: "Speaker Unit" },
+      { quantity: 2, item: "Speaker Cloth Panel" },
+      { quantity: 1, item: "User Manual" },
+      { quantity: 1, item: "3.5mm 10m Audio Cable" },
+      { quantity: 1, item: "10m Optical Cable" },
+    ],
+    button: "SEE PRODUCT",
+  },
 ];
 export default products;
